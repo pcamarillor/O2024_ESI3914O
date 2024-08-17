206b70eff916d687f022bd56f22a7d3e8f5239ff
Pablo Camarillo <p.camarillor@gmail.com>
2024-08-16 22:52:01 -0600

    Add Lab00

---
+++ Lab00/src/hello.py
@@ -0,0 +1,6 @@
+def hello_world():
+    return "Hello, World!"
+
+if __name__ == "__main__":
+    print(hello_world())
+
---
+++ Lab00/test/test_hello.py
@@ -0,0 +1,6 @@
+import pytest
+from src.hello import hello_world
+
+def test_hello_world():
+    assert hello_world() == "Hello, World!"
+
