2f829e444328103bf5b643fe4a6db23eaac032a5
Horacio H <hernandezh4@hotmail.com>
2024-08-20 22:14:09 -0700

    Solution is done

---
+++ src/solution_lab00_HoracioHernandez.py
@@ -0,0 +1,13 @@
+def solution(n):
+    n.sort()
+    for i in range(len(n)-1):
+        diff = n[i+1] - n[i]
+        if diff > 1:
+            return n[i] + 1
+    return len(n)
+
+    #n.sort()
+    #for i in range(len(n)):
+    #   if i != n[i]:
+    #       return i
+    #return len(n)
