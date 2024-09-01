97462df3230a86ad642b1b85e3e2ad3349be75b5
Michellefj01 <michelle.figueroa.jacques@gmail.com>
2024-09-01 17:19:19 -0600

    Added filtering to lab02

--- src/lab02/solution_lab02_michelle_figueroa.py
+++ src/lab02/solution_lab02_michelle_figueroa.py
@@ -1,4 +1,5 @@
 def analyze_log(log_rdd):
+    og_rdd = log_rdd.filter(lambda line: line.strip() != "")
     ip_rdd = log_rdd.map(lambda line: line.split(' ')[0])
     ip_count_rdd = ip_rdd.map(lambda ip: (ip, 1))
     ip_counts = ip_count_rdd.reduceByKey(lambda a, b: a + b)
