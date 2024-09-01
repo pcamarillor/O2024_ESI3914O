334636a7d3950b3aac839c3eb92a7bc8ea8ffcad
Chelosgd <salgado.marcelo@outlook.com>
2024-09-01 10:09:51 -0600

    Lab02 solution log analyzer method

---
+++ src/lab02/solution_lab02_marcelo_salgado.py
@@ -0,0 +1,6 @@
+def analyze_log(log_rdd):
+    ip_rdd = log_rdd.map(lambda line: line.split(' ')[0])
+    ip_count_rdd = ip_rdd.map(lambda ip: (ip, 1))
+    ip_counts = ip_count_rdd.reduceByKey(lambda a, b: a + b)
+    result = ip_counts.collectAsMap()
+    return result
