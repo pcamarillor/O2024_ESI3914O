1b79733b916867afd294b78a5b0df8b8e04d614c
Isaac De la Cruz <isaacdelacruz@Isaacs-MacBook-Pro.local>
2024-09-01 17:57:43 -0600

    Removed unnecessary code in solution file and added validation to remove blanks

--- src/lab02/solution_lab02_isaac_delacruz.py
+++ src/lab02/solution_lab02_isaac_delacruz.py
@@ -1,21 +1,3 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder \
-  .master("local[*]") \
-  .appName("ITESO-2024-SparkIntroduction") \
-  .config("spark.driver.bindAddress","localhost") \
-  .config("spark.ui.port","4040") \
-  .getOrCreate()
-
-sc = spark.sparkContext
-
-log_file = open("./../../datasets/access.log")
-log_data = log_file.read().splitlines()
-
-log_rdd = sc.parallelize(log_data)
-
 def analyze_log(log_rdd):
-    ip_count = log_rdd.map(lambda line: (line.split(" ")[0], 1)).reduceByKey(lambda a, b: a + b)
+    ip_count = log_rdd.filter(lambda line: line != "").map(lambda line: (line.split(" ")[0], 1)).reduceByKey(lambda a, b: a + b)
     return ip_count.collectAsMap()
-
-print(analyze_log(log_rdd))
