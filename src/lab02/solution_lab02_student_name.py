63952320a982eb3cfffa2df368aadaeab3d8a8db
nathannik123 <nathannik123@gmail.com>
2024-09-01 19:07:00 -0600

    Lab01}

--- src/lab02/solution_lab02_student_name.py
+++ src/lab02/solution_lab02_student_name.py
@@ -1,3 +1,14 @@
 def analyze_log(log_rdd):
-    # Write your solution here
-    return None
+    # Filter the missing lines
+    filtered_rdd = log_rdd.filter(lambda line: len(line.split()) > 0)
+
+    # Count the 
+    ip_rdd = filtered_rdd.map(lambda line: (line.split()[0], 1))
+
+    # ID groups
+    ip_count_rdd = ip_rdd.reduceByKey(lambda a, b: a + b)
+
+    # RDD to dic
+    ip_count = ip_count_rdd.collectAsMap()
+
+    return ip_count
