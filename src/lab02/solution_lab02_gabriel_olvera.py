137bf3624490b371d8c5f25b4cecacbe3e97048b
Gabriel Olvera <42094866+Gabored@users.noreply.github.com>
2024-09-03 17:17:29 -0700

    Fix solution_lab02_gabriel_olvera.py
    Fix using a non empty lines rdd strip

--- src/lab02/solution_lab02_gabriel_olvera.py
+++ src/lab02/solution_lab02_gabriel_olvera.py
@@ -1,6 +1,8 @@
 def analyze_log(log_rdd):
-    ip_rdd = log_rdd.map(lambda line: line.split(' ')[0])
+    non_empty_lines_rdd = log_rdd.filter(lambda line: line.strip() != '')
+    ip_rdd = non_empty_lines_rdd.map(lambda line: line.split(' ')[0])
     ip_count_rdd = ip_rdd.map(lambda ip: (ip, 1))
     ip_counts = ip_count_rdd.reduceByKey(lambda a, b: a + b)
+    
     result = ip_counts.collectAsMap()
     return result
