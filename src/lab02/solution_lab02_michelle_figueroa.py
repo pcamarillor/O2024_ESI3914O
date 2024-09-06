95c0f4d096ffbd3686738bb3a442ad681d25163c
Michellefj01 <99991287+Michellefj01@users.noreply.github.com>
2024-09-05 18:54:25 -0600

    Solution lab02_michelle_figueroa.py

--- src/lab02/solution_lab02_michelle_figueroa.py
+++ src/lab02/solution_lab02_michelle_figueroa.py
@@ -1,6 +1,6 @@
 def analyze_log(log_rdd):
     og_rdd = log_rdd.filter(lambda line: line.strip() != "")
-    ip_rdd = log_rdd.map(lambda line: line.split(' ')[0])
+    ip_rdd = og_rdd.map(lambda line: line.split(' ')[0])
     ip_count_rdd = ip_rdd.map(lambda ip: (ip, 1))
     ip_counts = ip_count_rdd.reduceByKey(lambda a, b: a + b)
     result = ip_counts.collectAsMap()
