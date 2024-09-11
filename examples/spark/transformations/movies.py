4b02a13c79062fc1fe2c9a258485b9fa3ac22698
Isaac De la Cruz <isaacdelacruz@DESKTOP-0H88MHG.ds.iteso.mx>
2024-09-10 19:19:37 -0600

    hello

--- examples/spark/transformations/movies.py
+++ examples/spark/transformations/movies.py
@@ -6,8 +6,12 @@ df_movies = spark.read \
             .option("inferSchema", "true") \
             .json("../../../datasets/movies.json")
 
-df_movies.printSchema()
-df_movies.show(n=7)
+# df_movies.printSchema()
+# df_movies.show(n=7)
 
+df_movies_filtered = df_movies.filter(df_movies.IMDB_Votes.isNotNull() & df_movies.Major_Genre.isNotNull())
+imdb_avg_by_genre = df_movies_filtered.groupBy("Major_Genre").avg("IMDB_Rating")
+
+imdb_avg_by_genre.show(n=20)
 
 spark.stop()
