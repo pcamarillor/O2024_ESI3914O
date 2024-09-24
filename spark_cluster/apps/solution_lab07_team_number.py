dbddd4bf1309a52c8d490e0cb1d44139ccdcc872
Pablo Camarillo <p.camarillor@gmail.com>
2024-09-23 18:34:37 -0600

    Add example for lecture 13: Storage Solutions Part I Add code base for lab 07: Persiting Netflix data

---
+++ spark_cluster/apps/solution_lab07_team_number.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame
+import datetime
+from pyspark.sql.functions import col, isnan, when, count, to_date
+
+def clean_df(netflix_df) -> DataFrame:
+    return None
+
+def write_df(netflix_df) -> None :
+    return None
+
+if __name__ == "__main__":
+    # Initialize SparkSession
+    spark = SparkSession \
+                    .builder \
+                    .appName("Lab 06 - RentalCarAnalysis") \
+                    .config("spark.ui.port","4040") \
+                    .getOrCreate()
+    
+    spark.sparkContext.setLogLevel("ERROR")
+
+    # Load Netflix DF
+    netflix_df = spark.read \
+            .option("header", "true") \
+            .option("inferSchema", "true") \
+            .csv("/opt/spark-data/netflix1.csv")
+    df_nulls = netflix_df \
+                .select([count(when(isnan(c) | col(c).isNull(), c)).alias(c) for c in netflix_df.columns])
+    
+    df_nulls.show()
+
+    netflix_df = clean_df(netflix_df)
+    
+    netflix_df.select([count(when(col(c).isNull(), c)).alias(c) for c in netflix_df.columns]).show()
+
+    write_df(netflix_df) 
