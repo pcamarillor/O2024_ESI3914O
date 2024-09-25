2693a18e5923daebf4ad81abd6fb0143d1d370d6
lmfontes19 <142419370+lmfontes19@users.noreply.github.com>
2024-09-24 22:41:49 -0600

    Fix queries

--- src/lab06/solution_lab06_team_5.py
+++ src/lab06/solution_lab06_team_5.py
@@ -8,42 +8,59 @@ def register_data_frames(cars_df, brands_df, customers_df, agencies_df, rentals_
     rentals_df.createOrReplaceTempView("rentals")
 
 def get_three_most_popular_car_brands(spark) -> DataFrame:
-   return spark.sql("SELECT b.brand_name, COUNT(r.rental_count) AS rental_count \
-        FROM cars c  \
+    df = spark.sql("SELECT b.brand_name, COUNT(r.rental_id) AS rental_count \
+        FROM rentals r \
+        JOIN cars c ON r.car_id = c.car_id \
         JOIN brands b ON c.brand_id = b.brand_id \
-        JOIN rentals r ON c.car_id = r.car_id \
         GROUP BY b.brand_name \
-        ORDER BY rental_count DESC").show(n=5, truncate=False)
+        ORDER BY rental_count DESC \
+        LIMIT 3")
+
+    df.show(n=5, truncate=False)
+    return df
 
 def calculate_total_revenue(spark) -> DataFrame:
-    return spark.sql("SELECT a.agency_name, SUM(c.price_per_day * DATEDIFF(r.return_date, r.rental_date)) AS total_revenue \
-        FROM cars c  \
+    df = spark.sql("SELECT a.agency_name, SUM(c.price_per_day * DATEDIFF(r.return_date, r.rental_date)) AS total_revenue \
+        FROM rentals r \
+        JOIN cars c ON r.car_id = c.car_id \
         JOIN agencies a ON c.agency_id = a.agency_id \
-        JOIN rentals r ON c.car_id = r.car_id \
-        GROUP BY b.brand_name \
+        GROUP BY a.agency_name \
         ORDER BY total_revenue DESC \
-        LIMIT 5").show(truncate=False)
+        LIMIT 5")
+
+    df.show(truncate=False)
+    return df
 
 def find_top_5_customers(spark) -> DataFrame:
-    return spark.sql("SELECT c.customer_name, SUM(c.price_per_day * DATEDIFF(r.return_date, r.rental_date)) AS total_spent \
-        FROM customers c  \
-        JOIN rentals r ON c.customer_id = r.customer_id \
-        JOIN cars c2 ON r.car_id = c2.car_id \
-        GROUP BY c.customer_name \
-        ORDER BY total_spen DESC").show(n=5, truncate=False)
+    df = spark.sql("SELECT cu.customer_name, SUM(ca.price_per_day * DATEDIFF(r.return_date, r.rental_date)) AS total_spent \
+        FROM customers cu \
+        JOIN rentals r ON cu.customer_id = r.customer_id \
+        JOIN cars ca ON ca.car_id = r.car_id \
+        GROUP BY cu.customer_name \
+        ORDER BY total_spent DESC \
+        LIMIT 5")
+
+    df.show(n=5, truncate=False)
+    return df
 
 def get_avg_age_customers_by_city(spark) -> DataFrame:
-    return spark.sql("SELECT c.city, AVG(c.age) AS average_age \
+    df = spark.sql("SELECT c.city, AVG(c.age) AS average_age \
         FROM customers c \
         JOIN rentals r ON c.customer_id = r.customer_id \
         GROUP BY c.city  \
         ORDER BY average_age DESC \
-        LIMIT 5").show(truncate=False) 
+        LIMIT 10")
+
+    df.show(truncate=False)
+    return df
 
 def find_car_models_most_revenue(spark) -> DataFrame:
-    return spark.sql("SELECT c.car_name, SUM(c.price_per_day * DATEDIFF(r.return_date, r.rental_date)) AS revenue \
+    df = spark.sql("SELECT c.car_name, SUM(c.price_per_day * DATEDIFF(r.return_date, r.rental_date)) AS revenue \
         FROM cars c \
         JOIN rentals r ON c.car_id = r.car_id \
         GROUP BY c.car_name \
         ORDER BY revenue DESC \
-        LIMIT 10").show(truncate=False)  
+        LIMIT 5")
+
+    df.show(truncate=False)
+    return df
