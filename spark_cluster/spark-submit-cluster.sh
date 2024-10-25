8eee54ab9aa81d0edafc59bc28648399d044c7a6
FernandoDeSantosFranco <fernando_desantos_franco@hotmail.com>
2024-10-24 19:22:00 -0600

    Submission lab 08

--- spark_cluster/spark-submit-cluster.sh
+++ spark_cluster/spark-submit-cluster.sh
@@ -8,3 +8,12 @@ docker exec -it spark_submit_container /spark/bin/spark-submit \
 --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.2 \
 /opt/spark-apps/$2 --kafka-bootstrap $3
 
+
+docker exec -it spark_submit_container /spark/bin/spark-submit --master spark://ea1f6426d52c:7077 --deploy-mode client --packages org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.2 /opt/spark-apps/stream_window_aggregations.py --kafka-bootstrap a11ae9e81b04
+
+
+docker run -d --name kafka_producer --network spark_cluster_default --volumes-from spark_cluster-spark-master-1 kafka-producer-app /bin/bash -c "sleep infinity"
+
+
+# Run producer
+docker exec -it kafka_producer python3 /opt/spark-apps/kafka_producers/kafka_sensor_producer.py --kafka-bootstrap a11ae9e81b04 --kafka-topic pyspark-example
