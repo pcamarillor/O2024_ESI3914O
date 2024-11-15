d6b2179b86de478d753b489bdd5fa056818c3eb8
Isaac De la Cruz <isaacdelacruz@Isaacs-MacBook-Pro.local>
2024-11-14 20:13:27 -0600

    Almost

--- spark_cluster/apps/graphX/page_rank.py
+++ spark_cluster/apps/graphX/page_rank.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from graphframes import GraphFrame
+from pyspark.sql.functions import concat, lit, split
 import random
 
 # Initialize SparkSession
@@ -24,26 +25,20 @@ vertices = vertices.drop("value")
 vertices = vertices.drop("timestamp")
 vertices = vertices.drop("rating")
 
-vertices = vertices.withColumn("userID", vertices["userID"].cast("int"))
+vertices = vertices.withColumn("id", vertices["userID"].cast("int"))
 vertices = vertices.withColumn("movieID", vertices["movieID"].cast("int"))
+vertices = vertices.withColumn("MovieStr", concat(lit("M-"), vertices["movieID"].cast("string")))
 vertices.show(n=size)
 
-edges = vertices.select(vertices["userID"].alias("src"), vertices["movieID"].alias("dst"))
-edges = edges.withColumn("src", edges["src"].cast("int"))
-edges = edges.withColumn("dst", edges["src"].cast("int"))
+edges = vertices.select(vertices["userID"].alias("src"), vertices["MovieStr"].alias("dst"))
 
 print("Edges as DataFrame:")
-edges.show(n=20)
+edges.show(n=150)
 
-# Create a GraphFrame
 graph = GraphFrame(vertices, edges)
 print("GraphFrame:")
 graph.vertices.show()
 graph.edges.show(n=size, truncate=False)
-
-# Query: Count the number of "married" connections in the graph.
-number_follow = graph.edges.filter("relationship = 'married'").count()
-print("Number of 'married' connections in the graph: {0}".format(number_follow))
 
 pr = graph.pageRank(resetProbability=0.85, maxIter=20)
 pr.vertices.select("id", "pagerank").orderBy("pagerank", ascending=False).show()
