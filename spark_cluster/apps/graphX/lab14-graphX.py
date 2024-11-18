d88a69e84f40c118f3904bee5725f138dca1efac
Francisco Flores <is728238@iteso.mx>
2024-11-18 16:31:10 -0600

    Lab14

---
+++ spark_cluster/apps/graphX/lab14-graphX.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from graphframes import GraphFrame
+
+spark = SparkSession.builder \
+    .appName("MovieInteractionAnalysis") \
+    .config("spark.jars.packages", "graphframes:graphframes:0.8.3-spark3.5-s_2.13") \
+    .getOrCreate()
+
+data = spark.read.csv("/opt/spark-data/sample_movielens_ratings.txt", sep="::", inferSchema=True)
+data = data.withColumnRenamed("_c0", "user") \
+           .withColumnRenamed("_c1", "movie") \
+           .withColumnRenamed("_c2", "rating")
+
+vertices = data.select("movie").distinct().withColumnRenamed("movie", "id")
+edges = data.select("user", "movie").withColumnRenamed("user", "src").withColumnRenamed("movie", "dst")
+
+graph = GraphFrame(vertices, edges)
+
+pr = graph.pageRank(resetProbability=0.85, maxIter=20)
+pr.vertices.orderBy("pagerank", ascending=False).show()
+
+spark.stop()
